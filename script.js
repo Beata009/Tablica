4db948b266a59d495a337b182165c33dea352e0d
Beata009 <bkolodziejszymanska@gmail.com>
2024-06-06 20:49:54 +0200

    Tablica

---
+++ script.js
@@ -0,0 +1,14 @@
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+
+console.log("Zawartość tablicy fruits:");
+for (const fruit of fruits) {
+    console.log(fruit);
+}
+const numbers = [2, 4, 6, 8, 1, 7];
+
+for (const number of numbers) {
+    if (number > 5) {
+        console.log(number + " jest większe niż 5");
+    }
+}
+
